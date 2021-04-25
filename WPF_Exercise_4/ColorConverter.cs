85dddc1b0795895b5ae6fe74dfde15715aed0f84
Pavlova Marina <marizza.mil89@gmail.com>
2021-04-25 23:54:51 +0300

    Add project files.

---
+++ WPF_Exercise_4/ColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WPF_Exercise_4
+{
+    class ColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            SolidColorBrush brush = new SolidColorBrush(Colors.Black);
+            double value = System.Convert.ToDouble(values[0]);
+            double maximum = System.Convert.ToDouble(values[1]);
+
+            if ((value / maximum) * 100 > 90)
+                brush = new SolidColorBrush(Colors.Red);
+
+            return brush;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPF_Exercise_4/PercentConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPF_Exercise_4
+{
+    class PercentConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double value = System.Convert.ToDouble(values[0]);
+            double maximum = System.Convert.ToDouble(values[1]);
+            return (value / maximum) * 100;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
